52d0fb83ae8ff0c10b3db2ede48e9faf819532cd
SagarWadikar <wadikar.sagar@gmail.com>
2023-07-11 23:37:58 +0530

    updated

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-import os
 
-MODEL_FILE_PATH = os.path.join(r'static','models', 'logistic_regression.pkl')
-JSON_FILE_PATH = os.path.join(r'static', 'json_files', 'proj_data.json')
+MODEL_FILE_PATH = 'static/models/logistic_regression.pkl'
+JSON_FILE_PATH = 'static/json_files/proj_data.json'
+
 
 PORT_NUMBER = 8080
