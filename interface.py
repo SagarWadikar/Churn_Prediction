3b1b7a7741da671ed3bb713dd9c024150f28fb3c
SagarWadikar <138306881+SagarWadikar@users.noreply.github.com>
2023-07-29 09:37:13 +0530

    Update interface.py

--- interface.py
+++ interface.py
@@ -1,86 +1,61 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, redirect, url_for
+import numpy as np
 from utils import ChurnPrediction
+import traceback
 import config
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/churn_prediction")
 def home():
     
     return render_template('index.html')
 
-@app.route("/predict_churn", methods= ['POST'])
+@app.route("/predict_churn", methods=['GET', 'POST'])
 def predict_churn():
-
-    data = request.form
-
-    credit_score = int(data["credit_score"])
-    gender = data["gender"]
-    age = int(data["age"])
-    tenure = int(data["tenure"])
-    balance = float(data["balance"])
-    products_number = int(data["products_number"])
-    credit_card = int(data["credit_card"])
-    active_member = int(data["active_member"])
-    estimated_salary = float(data["estimated_salary"])
-    country = data["country"]
-             
-    obj = ChurnPrediction(credit_score,gender,age,tenure,balance,products_number,credit_card,active_member,estimated_salary,country)
-             
-    pred_churn = obj.get_predicted_churn()
+    try:
+        if request.method == 'GET':
+            data = request.args.get
             
-    return render_template('index.html', prediction = pred_churn)
+            credit_score = float(data("credit_score"))
+            gender = data("gender")
+            age = int(data("age"))
+            tenure = int(data("tenure"))
+            balance = float(data("balance"))
+            products_number = int(data("products_number"))
+            credit_card = int(data("credit_card"))
+            active_member = int(data("active_member"))
+            estimated_salary = float(data("estimated_salary"))
+            country = data("country")
 
+            obj = ChurnPrediction(credit_score,gender,age,tenure,balance,products_number,credit_card,active_member,estimated_salary,country)
+            pred_churn = obj.get_predicted_churn()
+
+            return jsonify({"Result":f"Predicted customer churn == {pred_churn}"})
+        
+        elif request.method == 'POST':
+            data = request.form.get
+            print("Data :",data)
+
+            credit_score = float(data("credit_score"))
+            gender = data("gender")
+            age = int(data("age"))
+            tenure = int(data("tenure"))
+            balance = float(data("balance"))
+            products_number = int(data("products_number"))
+            credit_card = int(data("credit_card"))
+            active_member = int(data("active_member"))
+            estimated_salary = float(data("estimated_salary"))
+            country = data("country")
+
+            obj = ChurnPrediction(credit_score,gender,age,tenure,balance,products_number,credit_card,active_member,estimated_salary,country)
+            pred_churn = obj.get_predicted_churn()
+            
+            return render_template('index.html', prediction = pred_churn)
+
+    except:
+        print(traceback.print_exc())
+        return redirect(url_for('home'))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=config.PORT_NUMBER, debug=False)
-
-
-
-
-
-#              # if request.method == 'GET':
-#         #     data = request.args.get
-            
-#         #     credit_score = int(data["credit_score"])
-#         #     gender = data("gender")
-#         #     age = int(data["age"])
-#         #     tenure = int(data("tenure"))
-#         #     balance = float(data("balance"))
-#         #     products_number = int(data("products_number"))
-#         #     credit_card = int(data("credit_card"))
-#         #     active_member = int(data("active_member"))
-#         #     estimated_salary = float(data("estimated_salary"))
-#         #     country = data("country")
-
-#         #     obj = ChurnPrediction(credit_score,gender,age,tenure,balance,products_number,credit_card,active_member,estimated_salary,country)
-#         #     pred_churn = obj.get_predicted_churn()
-
-#         #     return jsonify({"Result":f"Predicted customer churn == {pred_churn}"})
-        
-#         # elif request.method == 'POST':
-#         #     data = request.form.get
-#         #     print("Data :",data)
-
-#         #     credit_score = int(data["credit_score"])
-#         #     gender = data("gender")
-#         #     age = int(data("age"))
-#         #     tenure = int(data("tenure"))
-#         #     balance = float(data("balance"))
-#         #     products_number = int(data("products_number"))
-#         #     credit_card = int(data("credit_card"))
-#         #     active_member = int(data("active_member"))
-#         #     estimated_salary = float(data("estimated_salary"))
-#         #     country = data("country")
-
-# credit_score = int(data("credit_score", 0))
-#             gender = int(data("gender",'0'))
-#             age = int(data("age", 0))
-#             tenure = int(data("tenure", 0))
-#             balance = float(data("balance", 0.0))
-#             products_number = int(data("products_number", 0))
-#             credit_card = int(data("credit_card", 0))
-#             active_member = int(data("active_member", 0))
-#             estimated_salary = float(data("estimated_salary", 0.0))
-#             # country = data("country")
-#             country = data.get("country", "")
